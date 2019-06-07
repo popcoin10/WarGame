95b5f172ad86166d7cceff06ca629718d74d96f0
mn10 <mattrn10@gmail.com>
2019-06-07 16:03:17 -0400

    test for war project

---
+++ src/wargame/WarGame.java
@@ -0,0 +1,16 @@
+package wargame;
+
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class WarGame
+{
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+
+   }
+}
