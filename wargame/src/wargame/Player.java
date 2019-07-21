ac8500a6e90fe003e7f86d956d5a34dde88aee30
portiaocran <portiaocran3@gmail.com>
2019-07-21 06:17:03 -0400

    hello

---
+++ wargame/src/wargame/Player.java
@@ -0,0 +1,24 @@
+package wargame;
+
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as a modifier and the date! * */
+
+import java.util.LinkedList;
+
+public class Player
+{
+   private LinkedList<Card> deck;
+
+   public Player (LinkedList<Card> deck)
+   {
+      this.deck = deck;
+   }
+   //testt
+
+   public LinkedList<Card> getDeck ()
+   {
+      return this.deck;
+   }
+}
